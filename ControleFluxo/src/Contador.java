44650e3791406d4e4a8a349753e22143f26f030a
Caio <calderarocaio@gmail.com>
2024-10-23 12:16:39 -0400

    qwe

--- ControleFluxo/src/Contador.java
+++ ControleFluxo/src/Contador.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
 
-public class Contador {
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
-		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		
-		}
-		catch(ParametrosInvalidosException){
-			System.out.println("O Segundo parâmetro deve ser maior que o primeiro");
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-		}
-		
-	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		if parametroUm > parametroDois
-			for contagem
+// Definindo a exceção personalizada
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
 
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-			
-		//realizar o for para imprimir os números com base na variável contagem
-	}
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+        
+        try {
+            // Chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        } 
+        catch (ParametrosInvalidosException e) {
+            // Imprimir a mensagem de erro quando a exceção for lançada
+            System.out.println(e.getMessage());
+        }
+        
+        terminal.close(); // Fechando o Scanner
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é maior que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        // Realizar o for para imprimir os números com base na variável de contagem
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número: " + i);
+        }
+    }
 }
--- ControleFluxo/src/ParametrosInvalidosException.java
+++
@@ -1,6 +0,0 @@
-public class ParametrosInvalidosException {
-    public static void main(String[] args){
-        System.out.println("insira nov");
-    }
-
-}
